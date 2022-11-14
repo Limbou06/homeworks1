a8d8e3cd32ff20979bcd38913e228b2821cd29ec
Magomed-Amin <99018983+Limbou06@users.noreply.github.com>
2022-11-14 23:00:41 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,11 @@
+def function(x) :
+    return ord(x)
+
+
+
+assert(function('A')) == 65
+assert(function('B')) == 66
+assert(function('C')) == 67
+assert(function('Z')) == 90
+assert(function('a')) == 97
+assert(function('z')) == 122
